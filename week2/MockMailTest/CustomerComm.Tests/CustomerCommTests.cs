6544e4a3c6b7cd0fb2667d4abd112784a152a186
Ritaja <tonnitarafder2004@gmail.com>
2025-06-29 12:40:29 +0530

    Add MockMailTest with NUnit test and Moq for week2 assignment

---
+++ week2/MockMailTest/CustomerComm.Tests/CustomerCommTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using Moq;
+using CustomerCommLib;
+
+namespace CustomerCommTests
+{
+    [TestFixture]
+    public class CustomerCommTests
+    {
+        private Mock<IMailSender> mockMailSender;
+        private CustomerComm customerComm;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            mockMailSender = new Mock<IMailSender>();
+            mockMailSender.Setup(x => x.SendMail(It.IsAny<string>(), It.IsAny<string>())).Returns(true);
+            customerComm = new CustomerComm(mockMailSender.Object);
+        }
+
+        [Test]
+        public void SendMailToCustomer_ShouldReturnTrue_WhenMailIsSent()
+        {
+            bool result = customerComm.SendMailToCustomer();
+            Assert.That(result, Is.True);
+        }
+    }
+}
