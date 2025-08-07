86661d07620518870d2d5de742aff13b771143e5
Ritaja <tonnitarafder2004@gmail.com>
2025-08-07 11:47:46 +0530

    Added React HANDSON 5

---
+++ week6/cohorttracker/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import { CohortsData } from './Cohort'
+import CohortDetails from './CohortDetails';
+
+function App() {
+
+  return (
+    <div style={{ border: '1px solid black', padding: '20px' }}>
+      <h1>Cohorts Details</h1>
+      {CohortsData.map((cohort, index) => (
+        <CohortDetails key={index} cohort={cohort} />
+      ))}
+    </div>
+  );
+}
+
+export default App;
